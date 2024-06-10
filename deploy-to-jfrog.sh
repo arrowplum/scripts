15b49b50b0b45240e9f9e379b683edb3e85664c6
Joe Martin <jmartin@aerospike.com>
2024-06-10 15:08:45 -0700

    Working script but needs to remove site and fix docker

--- deploy-to-jfrog.sh
+++ deploy-to-jfrog.sh
@@ -11,18 +11,19 @@ DEFAULT_ARTIFACT_VERSION="0.4.0"
 DEFAULT_HELM_CHART_VERSION="0.2.0"
 DEFAULT_BASE_DIR="/build"
 SITE_NAME="your-site-name"
-SIGNING_KEY="~/.gnupg/private-keys-v1.d/E1C8F4013F91B73E9DD455762BF009A57D65148D.key"
+SIGNING_KEY="aerospike"
+SNYK_FILE=""
 
 # Usage function
 usage() {
-  echo "Usage: $0 [-r release_number] [-a artifact_version] [-c helm_chart_version] [-b base_directory] [--release-number release_number] [--artifact-version artifact_version] [--helm-chart-version helm_chart_version] [--base-directory base_directory] [--site-name site_name] [--signing-key signing_key] [-h|--help]"
-  exit 1
+echo "Usage: $0 [-r release_number] [-a artifact_version] [-c helm_chart_version] [-b base_directory] [--release-number release_number] [--artifact-version artifact_version] [--helm-chart-version helm_chart_version] [--base-directory base_directory] [--site-name site_name] [--signing-key signing_key] [--snyk-file snyk_file] [-h|--help]"
+exit 1
 }
 
 # Parse command-line options using getopt
-OPTIONS=$(getopt -o r:a:c:b:s:k:h --long release-number:,artifact-version:,helm-chart-version:,base-directory:,site-name:,signing-key:,help -- "$@")
+OPTIONS=$(getopt -o r:a:c:b:s:k:f:h --long release-number:,artifact-version:,helm-chart-version:,base-directory:,site-name:,signing-key:,snyk-file:,help -- "$@")
 if [ $? -ne 0 ]; then
-  usage
+usage
 fi
 
 eval set -- "$OPTIONS"
@@ -34,27 +35,37 @@ HELM_CHART_VERSION=$DEFAULT_HELM_CHART_VERSION
 BASE_DIR=$DEFAULT_BASE_DIR
 
 while true; do
-  case "$1" in
+case "$1" in
     -r|--release-number)
-      RELEASE_NUMBER="$2"; shift 2;;
+    RELEASE_NUMBER="$2"; shift 2;;
     -a|--artifact-version)
-      ARTIFACT_VERSION="$2"; shift 2;;
+    ARTIFACT_VERSION="$2"; shift 2;;
     -c|--helm-chart-version)
-      HELM_CHART_VERSION="$2"; shift 2;;
+    HELM_CHART_VERSION="$2"; shift 2;;
     -b|--base-directory)
-      BASE_DIR="$2"; shift 2;;
+    BASE_DIR="$2"; shift 2;;
     -s|--site-name)
-      SITE_NAME="$2"; shift 2;;
+    SITE_NAME="$2"; shift 2;;
     -k|--signing-key)
-      SIGNING_KEY="$2"; shift 2;;
+    SIGNING_KEY="$2"; shift 2;;
+    -f|--snyk-file)
+    SNYK_FILE="$2"; shift 2;;
     -h|--help)
-      usage; shift;;
+    usage; shift;;
     --)
-      shift; break;;
+    shift; break;;
     *)
-      usage;;
-  esac
+    usage;;
+esac
 done
+if [ -d "${BASE_DIR}/${RELEASE_NAME}/${RELEASE_NUMBER}" ]; then
+    echo "Error: Release ${RELEASE_NUMBER} already exists for ${RELEASE_NAME}. Cannot create a new release with the same version."
+    exit 1
+fi
+
+mkdir "${BASE_DIR}/${RELEASE_NAME}/${RELEASE_NUMBER}"
+cp -r ${BASE_DIR}/${RELEASE_NAME}/current/* ${BASE_DIR}/${RELEASE_NAME}/${RELEASE_NUMBER}
+
 
 # Define artifact names
 DOCKER_IMAGE_NAME="aerospike/aerospike-proximus:${ARTIFACT_VERSION}"
@@ -64,9 +75,10 @@ RPM_PACKAGE_NAME="aerospike-proximus-${ARTIFACT_VERSION}-1.noarch.rpm"
 DEB_PACKAGE_NAME="aerospike-proximus-${ARTIFACT_VERSION}.all.deb"
 SBOM_FILE_NAME="${RELEASE_NAME}-${ARTIFACT_VERSION}-sbom.json"
 SNYK_REPORT_NAME="snyk-report-${RELEASE_NAME}-${ARTIFACT_VERSION}.sarif"
-SPEC_FILE_NAME="spec-${RELEASE_NAME}-${RELEASE_NUMBER}.json"
-RELEASE_BUNDLE_SPEC_NAME="release-bundle-${RELEASE_NAME}-${RELEASE_NUMBER}.json"
-DISTRIBUTION_RULES_NAME="distribution-rules-${RELEASE_NAME}-${RELEASE_NUMBER}.json"
+SPEC_FILE_NAME="spec-${RELEASE_NAME}.json"
+RELEASE_BUNDLE_SPEC_NAME="release-bundle-${RELEASE_NAME}.json"
+DISTRIBUTION_RULES_NAME="distribution-rules-${RELEASE_NAME}.json"
+SNYK_FILE_NAME="$(basename ${SNYK_FILE})"
 
 # Compute paths based on release name, release number, and artifact names
 DOCKER_TAR="${BASE_DIR}/${RELEASE_NAME}/${RELEASE_NUMBER}/docker/${DOCKER_TAR_NAME}"
@@ -78,14 +90,16 @@ SNYK_REPORT="${BASE_DIR}/${RELEASE_NAME}/${RELEASE_NUMBER}/metadata/${SNYK_REPOR
 SPEC_FILE="${BASE_DIR}/${RELEASE_NAME}/${RELEASE_NUMBER}/metadata/${SPEC_FILE_NAME}"
 RELEASE_BUNDLE_SPEC="${BASE_DIR}/${RELEASE_NAME}/${RELEASE_NUMBER}/metadata/${RELEASE_BUNDLE_SPEC_NAME}"
 DISTRIBUTION_RULES="${BASE_DIR}/${RELEASE_NAME}/${RELEASE_NUMBER}/metadata/${DISTRIBUTION_RULES_NAME}"
+SNYK_FILE_DEST="${BASE_DIR}/${RELEASE_NAME}/${RELEASE_NUMBER}/metadata/snyk-config"
+SNYK_FILE_DOCKER_DIR="${BASE_DIR}/${RELEASE_NAME}/${RELEASE_NUMBER}/docker/.snyk"
 RELEASE_BUNDLE_NAME="${RELEASE_NAME}-${ARTIFACT_VERSION}"
 
 # Ensure required tools are installed
-for cmd in docker jf syft jq; do
-  if ! command -v $cmd &> /dev/null; then
+for cmd in docker jf syft jq snyk; do
+if ! command -v $cmd &> /dev/null; then
     echo "Error: $cmd is not installed." >&2
     exit 1
-  fi
+fi
 done
 
 # Pull Docker image and save to tar file
@@ -94,27 +108,19 @@ docker save -o ${DOCKER_TAR} ${DOCKER_IMAGE_NAME}
 
 # Function to get site ID with error handling
 get_site_id() {
-  local site_name="$1"
-  local response
-  local http_status
-  local site_id
+    local site_name="$1"
+    local response
+    local http_status
 
-  # Make the curl call and capture the HTTP status code
-  response=$(jf rt curl -X GET /distribution/api/v1/sites -o - -w "%{http_code}")
-  http_status=$(echo "${response: -3}")
-  response=$(echo "${response%???}")
+    # Make the curl call and capture the HTTP status code
+    response=$(jf rt curl -X GET /distribution/api/v1/sites -o - -w "%{http_code}")
+    http_status=$(echo "${response: -3}")
+    response=$(echo "${response%???}")
 
-  # Check if the HTTP status code is 200 (OK)
-  if [ "$http_status" -eq 200 ]; then
-    site_id=$(echo "$response" | jq -r --arg name "$site_name" '.[] | select(.name == $name) | .id')
-    if [ -z "$site_id" ]; then
-      echo "Site ID for '$site_name' not found in response."
-    else
-      echo "$site_id"
-    fi
-  else
-    echo "Error: Received HTTP status $http_status. Response: $response"
-  fi
+    # Check if the HTTP status code is 200 (OK)
+    if [ "$http_status" -eq 200 ]; then
+        echo "$response" | jq -r --arg name "$site_name" '.[] | select(.name == $name) | .id'
+fi
 }
 
 # Retrieve the site ID for the given site name
@@ -122,124 +128,112 @@ SITE_ID=$(get_site_id "$SITE_NAME")
 
 # Check if SITE_ID is empty
 if [ -z "$SITE_ID" ]; then
-  echo "No sites configured or site ID for '$SITE_NAME' not found. Skipping distribution."
-  DISTRIBUTE=false
+    echo "No sites configured or site ID for '$SITE_NAME' not found. Skipping distribution."
+    DISTRIBUTE=false
 else
-  echo "Site ID for '$SITE_NAME' is $SITE_ID"
-  DISTRIBUTE=true
+    echo "Site ID for '$SITE_NAME' is $SITE_ID"
+    DISTRIBUTE=true
 fi
 
-# # Run Snyk test and generate SARIF report
-snyk container test docker-archive:${DOCKER_TAR} --file=${DOCKER_TAR} --sarif-file-output=${SNYK_REPORT} ----sarif
+# Copy the Snyk file to the metadata directory and Docker tar directory
+if [ -n "$SNYK_FILE" ]; then
+    cp ${SNYK_FILE} ${SNYK_FILE_DEST}
+    cp ${SNYK_FILE} ${SNYK_FILE_DOCKER_DIR}
+fi
+
+# Run Snyk test and generate SARIF report
+# snyk container test docker-archive:${DOCKER_TAR} --sarif-file-output=${SNYK_REPORT} --file=${SNYK_FILE_DOCKER_DIR}
 
 # Generate SBOM using Syft
 syft ${DOCKER_TAR} -o json > ${SBOM_FILE}
-
+touch ${DOCKER_TAR} ${HELM_CHART} ${RPM_PACKAGE} ${DEB_PACKAGE} ${SBOM_FILE} ${SNYK_REPORT} ${SNYK_FILE_DEST} ${SPEC_FILE} ${RELEASE_BUNDLE_SPEC} ${DISTRIBUTION_RULES}
 # Create spec file dynamically after all files are created
+# and add any needed properties and coordinates (both in props)
+
 cat <<EOF > ${SPEC_FILE}
 {
-  "files": [
-    {
-      "pattern": "${DOCKER_TAR}",
-      "target": "ecosystem-container-dev-local/${RELEASE_NAME}/${ARTIFACT_VERSION}/"
-    },
-    {
-      "pattern": "${HELM_CHART}",
-      "target": "ecosystem-helm-dev-local/${RELEASE_NAME}/${HELM_CHART_VERSION}/"
-    },
-    {
-      "pattern": "${RPM_PACKAGE}",
-      "target": "ecosystem-rpm-dev-local/${RELEASE_NAME}/${ARTIFACT_VERSION}/"
-    },
-    {
-      "pattern": "${DEB_PACKAGE}",
-      "target": "ecosystem-deb-dev-local/${RELEASE_NAME}/${ARTIFACT_VERSION}/"
-    },
-    {
-      "pattern": "${SBOM_FILE}",
-      "target": "ecosystem-pkg-dev-local/${RELEASE_NAME}/${ARTIFACT_VERSION}/"
-    },
-    {
-      "pattern": "${SNYK_REPORT}",
-      "target": "ecosystem-pkg-dev-local/${RELEASE_NAME}/${ARTIFACT_VERSION}/"
-    },
-    {
-      "pattern": "${SPEC_FILE}",
-      "target": "ecosystem-pkg-dev-local/${RELEASE_NAME}/${ARTIFACT_VERSION}/"
-    },
-    {
-      "pattern": "${RELEASE_BUNDLE_SPEC}",
-      "target": "ecosystem-pkg-dev-local/${RELEASE_NAME}/${ARTIFACT_VERSION}/"
-    },
-    {
-      "pattern": "${DISTRIBUTION_RULES}",
-      "target": "ecosystem-pkg-dev-local/${RELEASE_NAME}/${ARTIFACT_VERSION}/"
-    }
-  ]
+    "files": [
+        {
+            "pattern": "${DOCKER_TAR}",
+            "target": "ecosystem-container-dev-local/${RELEASE_NAME}/${ARTIFACT_VERSION}/",
+            "props": "docker.repo=${DOCKER_IMAGE_NAME}"
+        },
+        {
+            "pattern": "${HELM_CHART}",
+            "target": "ecosystem-helm-dev-local/${RELEASE_NAME}/${HELM_CHART_VERSION}/",
+            "props": "helm.chart=${RELEASE_NAME};helm.version=${HELM_CHART_VERSION}"
+        },
+        {
+            "pattern": "${RPM_PACKAGE}",
+            "target": "ecosystem-rpm-dev-local/${RELEASE_NAME}/${ARTIFACT_VERSION}/",
+            "props": "rpm.name=${RELEASE_NAME};rpm.version=${ARTIFACT_VERSION}"
+        },
+        {
+            "pattern": "${DEB_PACKAGE}",
+            "target": "ecosystem-deb-dev-local/${RELEASE_NAME}/${ARTIFACT_VERSION}/",
+            "props": "deb.name=${RELEASE_NAME};deb.version=${ARTIFACT_VERSION};deb.distribution=stable;deb.component=main;deb.architecture=all"
+        },
+        {
+            "pattern": "${SBOM_FILE}",
+            "target": "ecosystem-meta-dev-local/${RELEASE_NAME}/${ARTIFACT_VERSION}/"
+        },
+        {
+            "pattern": "${SNYK_REPORT}",
+            "target": "ecosystem-meta-dev-local/${RELEASE_NAME}/${ARTIFACT_VERSION}/"
+        },
+        {
+            "pattern": "${SNYK_FILE_DEST}",
+            "target": "ecosystem-pkg-dev-local/${RELEASE_NAME}/${ARTIFACT_VERSION}/"
+        },
+        {
+            "pattern": "${SPEC_FILE}",
+            "target": "ecosystem-pkg-dev-local/${RELEASE_NAME}/${ARTIFACT_VERSION}/"
+        },
+        {
+            "pattern": "${RELEASE_BUNDLE_SPEC}",
+            "target": "ecosystem-pkg-dev-local/${RELEASE_NAME}/${ARTIFACT_VERSION}/"
+        },
+        {
+            "pattern": "${DISTRIBUTION_RULES}",
+            "target": "ecosystem-pkg-dev-local/${RELEASE_NAME}/${ARTIFACT_VERSION}/"
+        }
+    ]
 }
 EOF
-
 # Upload all artifacts using JF CLI
-jf rt upload --spec=${SPEC_FILE}
+jf rt upload --spec=${SPEC_FILE} --project=ecosystem
 
 # Create release bundle specification dynamically with supported fields
 cat <<EOF > ${RELEASE_BUNDLE_SPEC}
 {
-  "version": "${RELEASE_NUMBER}",
-  "release_notes": {
-    "syntax": "markdown",
-    "content": "Release notes for ${RELEASE_BUNDLE_NAME} version ${ARTIFACT_VERSION}"
-  },
-  "dry_run": false,
-  "sign_immediately": false,
-  "files": [
-    {
-      "pattern": "ecosystem-container-dev-local/${RELEASE_NAME}/${ARTIFACT_VERSION}/*"
+    "version": "${RELEASE_NUMBER}",
+    "release_notes": {
+        "syntax": "markdown",
+        "content": "Release notes for ${RELEASE_BUNDLE_NAME} version ${ARTIFACT_VERSION}"
     },
-    {
-      "pattern": "ecosystem-helm-dev-local/${RELEASE_NAME}/${HELM_CHART_VERSION}/*"
-    },
-    {
-      "pattern": "ecosystem-rpm-dev-local/${RELEASE_NAME}/${ARTIFACT_VERSION}/*"
-    },
-    {
-      "pattern": "ecosystem-deb-dev-local/${RELEASE_NAME}/${ARTIFACT_VERSION}/*"
-    },
-    {
-      "pattern": "ecosystem-pkg-dev-local/${RELEASE_NAME}/${ARTIFACT_VERSION}/*"
-    }
-  ]
+    "dry_run": false,
+    "sign_immediately": false,
+    "files": [
+        {
+        "pattern": "ecosystem-container-dev-local/${RELEASE_NAME}/${ARTIFACT_VERSION}/*"
+        },
+        {
+        "pattern": "ecosystem-helm-dev-local/${RELEASE_NAME}/${HELM_CHART_VERSION}/*"
+        },
+        {
+        "pattern": "ecosystem-rpm-dev-local/${RELEASE_NAME}/${ARTIFACT_VERSION}/*"
+        },
+        {
+        "pattern": "ecosystem-deb-dev-local/${RELEASE_NAME}/${ARTIFACT_VERSION}/*"
+        },
+        {
+        "pattern": "ecosystem-pkg-dev-local/${RELEASE_NAME}/${ARTIFACT_VERSION}/*"
+        }
+    ]
 }
 EOF
 
 # Create the release bundle
 jf release-bundle-create ${RELEASE_BUNDLE_NAME} ${RELEASE_NUMBER} \
-  --spec=${RELEASE_BUNDLE_SPEC} --signing-key=${SIGNING_KEY}
+--spec=${RELEASE_BUNDLE_SPEC} --signing-key=${SIGNING_KEY} --project=ecosystem
 
-# If sites are configured, create distribution rules and promote the release bundle to stage
-if [ "$DISTRIBUTE" = true ]; then
-  # Create distribution rules dynamically
-  cat <<EOF > ${DISTRIBUTION_RULES}
-{
-  "version": "${RELEASE_NUMBER}",
-  "rules": [
-    {
-      "name": "Promote to Stage",
-      "repositories": [
-        "ecosystem-container-stage-local",
-        "ecosystem-helm-stage-local",
-        "ecosystem-rpm-stage-local",
-        "ecosystem-deb-stage-local",
-        "ecosystem-pkg-dev-local"
-      ],
-      "site": "${SITE_NAME}"
-    }
-  ]
-}
-EOF
-
-  # Promote the release bundle to stage
-  jf  release-bundle-promote ${RELEASE_BUNDLE_NAME} ${RELEASE_NUMBER} --site=${SITE_NAME} --dist-rules=${DISTRIBUTION_RULES}
-else
-  echo "Skipping distribution as no sites are configured."
-fi
